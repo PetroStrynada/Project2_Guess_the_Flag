ea3d4105d2aff92c53d9954aca0a6d508f43cdfe
Petro Strynada <petro.strynada@gmail.com>
2023-08-02 21:41:04 +0300

    Challenge Project 12
    + save and load highest score
    Now, if you will set new record
    You will get alert message

---
+++ Project2_Guess_the_Flag/HighestScore.swift
@@ -0,0 +1,12 @@
+//
+//  HighestScore.swift
+//  Project2_Guess_the_Flag
+//
+//  Created by Petro Strynada on 02.08.2023.
+//
+
+import UIKit
+
+class HighestScore: NSObject, Codable {
+    var highestScore: Int
+}
